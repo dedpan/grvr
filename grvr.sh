d00fa42ac11e481a2d6d4a71d292f5cac9962fdc
dedpan <dedpan23@gmail.com>
2021-12-14 20:32:44 -0800

    v1

---
+++ grvr.sh
@@ -0,0 +1,10 @@
+wget -P $HOST/Downloads https://golang.org/dl/go$1.linux-amd64.tar.gz
+echo $1 "binary downloaded and stored in" $HOST/Downloads
+sudo tar -C /usr/local/ -xzf $HOST/Downloads/go$1.linux-amd64.tar.gz
+echo "Go installed! Now configuring the ENV variables!"
+echo "export GOROOT=/usr/local/go
+export GOPATH=\$HOME/go
+export PATH=\$GOPATH/bin:\$GOROOT/bin:\$PATH">> ~/.bashrc
+source ~/.bashrc
+echo "Go is now ready to use!
+    Confirm by running 'go version'"
